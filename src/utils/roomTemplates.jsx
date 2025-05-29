eb9558798e0ea756b91b20ab0f7c65932f26edf8
Xenoszz <Natthinan.safety@gmail.com>
2025-05-30 03:18:21 +0700

    [UPDATE] Image Response

--- src/utils/roomTemplates.jsx
+++ src/utils/roomTemplates.jsx
@@ -1,26 +1,26 @@
 export const roomTemplates = [
     {
         name: "Bedroom",
-        img: "/bedroom.jpg"
+        img: "https://homiehome.blob.core.windows.net/image/bedroom-d76fd35b-c15d-41f3-a3ba-817826dbffab.jpg"
     },
     {
         name: "Bathroom",
-        img: "/bathroom.jpg"
+        img: "https://homiehome.blob.core.windows.net/image/bathroom-0f5290ad-31e7-4dbd-9f99-24e753887120.jpg"
     },
     {
         name: "Kitchen",
-        img: "/kitchen.jpg"
+        img: "https://homiehome.blob.core.windows.net/image/kitchen-10568113-4dc3-42e5-8080-b93fa1336698.jpg"
     },
     {
         name: "Guest room",
-        img: "/guestroom.jpg"
+        img: "https://homiehome.blob.core.windows.net/image/guestroom-71511089-172b-4422-855d-fe453d4df22a.jpg"
     },
     {
         name: "Storage room",
-        img: "/storage.jpg"
+        img: "https://homiehome.blob.core.windows.net/image/storage-395cc436-d2b3-4126-9833-ab2896730b70.jpg"
     },
     {
         name: "Laundry room",
-        img: "/laundry.jpg"
+        img: "https://homiehome.blob.core.windows.net/image/laundry-73aad852-c0db-45fb-9461-b2bb8496e877.jpg"
     },
 ];
