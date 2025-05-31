3de48b15ffceeece72c49d0461fd1dcf6baaf745
Xenoszz <Natthinan.safety@gmail.com>
2025-05-31 15:28:55 +0700

    [UPDATE] Icon Header

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.jpg" type="image/jpg" sizes="32x32" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
